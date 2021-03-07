d44f25fb2af40a8097427ec9bcfa467e68b2e955
WesternGamer <Kevnguyen474@gmail.com>
2021-03-07 11:19:26 -0500

    Program Code Added

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Hash_Tutorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, place, adjective, emotion;
+
+            Console.WriteLine("Mad Lib Program by Western");
+
+            Console.Write("Enter a name: ");
+            name = Console.ReadLine();
+
+            Console.Write("Enter a place: ");
+            place = Console.ReadLine();
+
+            Console.Write("Enter a adjective: ");
+            adjective = Console.ReadLine();
+
+            Console.Write("Enter a emotion: ");
+            emotion = Console.ReadLine();
+
+            Console.WriteLine(name + " went to the " + place + ".");
+            Console.WriteLine("It was "+ adjective + "!");
+            Console.WriteLine("But it was " + emotion + " that " + name + " had to go home.");
+
+            Console.WriteLine("Press enter to exit.");
+
+            Console.ReadLine();
+        }
+    }
+}
